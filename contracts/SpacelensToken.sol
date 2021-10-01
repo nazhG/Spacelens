89dd27ec4cf674a36ab7d01765a28931179178e0
Nazhir Gonzalez Edbaissi <nazhirgonzalez@gmail.com>
2021-10-01 04:28:05 -0400

    contract and test

---
+++ contracts/SpacelensToken.sol
@@ -0,0 +1,118 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @title Spacelens ERC20
+/// @author nazhG
+/// @notice this contract allow create phases for mint token and transfer the funds
+contract SpacelensToken is ERC20, Ownable, Pausable {
+
+	/// a phase is a period for a discount in minting price 
+	/// until amount N of token sold out or reaching a date
+	/// @dev a phase is always needed to mint
+    struct Phase {
+		/// uint between 0 - 100 to mul with the price on sells
+        uint256 discount;
+		/// timestamp when this phase ends
+        uint256 end;
+		/// uint that decreases when sold in phase
+		/// @note to know the original supply look up in logs
+        uint256 supply;
+    }
+
+	/// all phases (next, current and previous) 
+    mapping(uint256 => Phase) public phases;
+
+	/// reference for the mapping of phases, uint of the current phase
+    uint256 public currentPhase;
+
+	/// reference for the mapping of phases, uint of the last phase
+	/// @dev phases[lastPhase - 1] to get the last phase
+    uint256 public lastPhase;
+
+	/// @notice max amount of token allowed to mint in this contract
+    uint256 public immutable maxSupply;
+
+	/// @notice current mintable amount of tokens
+    uint256 public supply;
+
+	/// @notice minimum amount of tokens that can be minted in one transaction
+    uint256 public minimumToken;
+
+	/// @notice amount of tokens minted for one ETH
+    uint256 public spacePrice;
+
+	/// @notice wallet to transfer funds of the contract
+    address public dispatcher;
+
+    constructor(
+        uint256 _maxSupply,
+        uint256 _spacePrice,
+        uint256 _minimumToken,
+        address _dispatcher
+    ) ERC20("Spacelens", "SPCL") {
+        currentPhase = 0;
+        lastPhase = 0;
+        maxSupply = _maxSupply;
+		supply = _maxSupply;
+        spacePrice = _spacePrice;
+        minimumToken = _minimumToken;
+        dispatcher = _dispatcher;
+    }
+
+	/// @notice add a phase to mapping
+	function createPhase(uint256 _discount, uint256 _end, uint256 _supply) external onlyOwner {
+		Phase storage p = phases[lastPhase++];
+
+		require(_discount < 101, "Discount cannot be greater than 100%");
+		p.discount = _discount;
+
+		require(block.timestamp < _end, "The end of the phase should be greater than now");
+		p.end = _end;
+
+		require(supply >= _supply, "not enough supply to mint");
+		/// supply will decrease with each phase
+		/// if the supply reaches 0 means that the cap of token are distributed in the phases
+		supply -= _supply;
+		p.supply = _supply;
+	}
+
+	/// @notice change account to transfer the contract balance
+	function changeDispatcher(address _dispatcher) external onlyOwner {
+        dispatcher = _dispatcher;
+	}
+
+	/// @notice mint tokens, require send ETH
+	function buySpacelens(uint256 _tokenAmount) external payable whenNotPaused {
+		/// advance phase if the time is out
+		if (block.timestamp > phases[currentPhase].end) {
+			currentPhase++;
+		}
+		require(phases[currentPhase].supply >= _tokenAmount, "not enought supply");
+		require(msg.value > 0, "not ETH");
+		/// calculation: tokens / (price * (100 - discount) / 100)
+		uint256 finalPrice = (_tokenAmount / (spacePrice * (100 - phases[currentPhase].discount))) / 100;
+		require(finalPrice <= msg.value, "not enough eth");
+		_mint(msg.sender, _tokenAmount);
+		/// change currenta phase total supply
+		phases[currentPhase].supply -= _tokenAmount;
+		/// advance phase if the supply is out
+		if (phases[currentPhase].supply == 0) {
+			currentPhase++;
+		}
+	}
+
+	/// @notice pause the mint, no one could buy token from this contract
+	function pauseMint() external onlyOwner {
+		_pause();
+    }
+
+	/// @notice continue minting, let user call function to buy token
+	function unpauseMint() external onlyOwner {
+		_unpause();
+    }
+
+}
---
+++ test/SpacelensToken.test.js
@@ -0,0 +1,218 @@
+const SpacelensToken = artifacts.require("SpacelensToken");
+const {
+  expectEvent,
+  expectRevert,
+  time,
+} = require("@openzeppelin/test-helpers");
+const { web3 } = require("@openzeppelin/test-helpers/src/setup");
+
+// this function include the desimals
+toBN = (num) => web3.utils.toBN(num + "0".repeat(18));
+
+toWei = (num) => web3.utils.toWei(num);
+fromWei = (num) => web3.utils.fromWei(num);
+
+contract("Spacelens Token", ([owner, user]) => {
+  let spaceToken;
+  const maxSupply = toBN(10000),
+    price = 5 /** 1 ETH = 5 Tokens */,
+    min = toBN(1);
+
+  before(async function () {
+    spaceToken = await SpacelensToken.new(maxSupply, price, min, owner, {
+      from: owner,
+    });
+  });
+
+  it("Init first phase", async function () {
+    const discount = 50 /** 50% */,
+      dateEndPhase =
+        Number(await time.latest()) + 3600 /** the phase will last one hour */,
+      supply = toBN(1000);
+
+    await spaceToken.createPhase(discount, dateEndPhase, supply, {
+      from: owner,
+    });
+
+    const phase = await spaceToken.phases(0);
+
+    /** checking that the phase is created */
+    assert.equal(Number(phase.discount), discount, "Phase discount err");
+    assert.equal(Number(phase.end), dateEndPhase, "Phase ends err");
+    assert.equal(Number(phase.supply), supply, "Phase supply err");
+    /** checking that the contract decrease the supply */
+    assert.equal(
+      Number(await spaceToken.supply()),
+      maxSupply - supply,
+      "Contract dicrease supply err"
+    );
+  });
+
+  it("Errors creating phases", async function () {
+    /// err 200% discount
+    await expectRevert(
+      spaceToken.createPhase(200, (await time.latest()) + 1, toBN(1), {
+        from: owner,
+      }),
+      "Discount cannot be greater than 100%"
+    );
+    /// err end date is now less one second
+    await expectRevert(
+      spaceToken.createPhase(1, (await time.latest()) - 1, toBN(1), {
+        from: owner,
+      }),
+      "The end of the phase should be greater than now"
+    );
+    /// err more that maxSupply (the current supply is maxSupply - phase one original supply)
+    await expectRevert(
+      spaceToken.createPhase(1, (await time.latest()) + 1, maxSupply, {
+        from: owner,
+      }),
+      "not enough supply to mint"
+    );
+  });
+
+  it("Init second phase", async function () {
+    const discount = 25,
+      // this phase end one hour after phase one over
+      dateEndPhase = Number((await spaceToken.phases(0)).end) + 3600,
+      supply = toBN(3000);
+
+    await spaceToken.createPhase(discount, dateEndPhase, supply, {
+      from: owner,
+    });
+
+    const phase = await spaceToken.phases(1);
+
+    /** checking that the phase is created */
+    assert.equal(Number(phase.discount), discount, "Phase discount err");
+    assert.equal(Number(phase.end), dateEndPhase, "Phase ends err");
+    assert.equal(Number(phase.supply), supply, "Phase supply err");
+    /** checking that the contract decrease the supply */
+    assert.equal(
+      Number(await spaceToken.supply()),
+      toBN(6000),
+      "Contract dicrease supply err"
+    );
+    /** checking how many phase are, and if the phase one is not finished */
+    assert.equal(
+      Number(await spaceToken.currentPhase()),
+      0,
+      "Phase one is over err"
+    );
+    assert.equal(
+      Number(await spaceToken.lastPhase()),
+      2,
+      "Should be only 2 phases"
+    );
+  });
+
+  it("Should mint token", async function () {
+    const currentPhaseNumber = Number(await spaceToken.currentPhase());
+
+    const preUserBalance = Number(await spaceToken.balanceOf(user));
+    const prePhaseSupply = Number(
+      (await spaceToken.phases(currentPhaseNumber)).supply
+    );
+    await spaceToken.buySpacelens(toBN("400"), {
+      from: user,
+      value: toWei("160.5"),
+    });
+    const postUserBalance = Number(await spaceToken.balanceOf(user));
+    const posPhaseSupply = Number(
+      (await spaceToken.phases(currentPhaseNumber)).supply
+    );
+    assert.equal(preUserBalance, 0, "user phase one pre balance err");
+
+    /// check the user have the token
+    assert.equal(
+      postUserBalance,
+      Number(toBN("400")),
+      "user phase one pos balance err"
+    );
+    /// check the phase supply decrase
+    assert.equal(
+      prePhaseSupply,
+      posPhaseSupply + Number(toBN("400")),
+      "supply phase one balance err"
+    );
+    /// check that the phase not change yet
+    assert.equal(
+      Number(await spaceToken.currentPhase()),
+      currentPhaseNumber,
+      "current phase err"
+    );
+  });
+
+  it("Should end the phase (supply out)", async function () {
+    const currentPhaseNumber = Number(await spaceToken.currentPhase());
+
+    // cal the ETH needed to this operation
+    const ethNeeded =
+      Number((await spaceToken.phases(currentPhaseNumber)).supply) /
+      (Number(await spaceToken.spacePrice()) *
+        (100 -
+          Number((await spaceToken.phases(currentPhaseNumber)).discount))) /
+      100;
+    /// err not enought ETH
+    await expectRevert(
+      spaceToken.buySpacelens(
+        (
+          await spaceToken.phases(currentPhaseNumber)
+        ).supply,
+        { from: user, value: ethNeeded - 10 }
+      ),
+      "not enough eth"
+    );
+
+    await spaceToken.buySpacelens(
+      (
+        await spaceToken.phases(currentPhaseNumber)
+      ).supply,
+      { from: user, value: ethNeeded }
+    );
+
+    // check that the initial phase is not the current phase
+    assert.notEqual(
+      Number(await spaceToken.currentPhase()),
+      currentPhaseNumber,
+      "change phase per supply out err"
+    );
+
+    // check supply
+    assert.notEqual(
+      Number((await spaceToken.phases(1)).supply),
+      0,
+      "phase supply err"
+    );
+    assert.equal(
+      Number((await spaceToken.phases(0)).supply),
+      0,
+      "phase supply err"
+    );
+  });
+
+  it("Should end the phase (time out)", async function () {
+	/// advance time 4 hours to end the phase
+    await time.increase(time.duration.hours(4)); 
+	
+	/// create the last phase (3)
+    const discount = 0,
+	dateEndPhase =
+	Number(await time.latest()) + time.duration.years(1),
+	supply = (await await spaceToken.supply());
+    await spaceToken.createPhase(discount, dateEndPhase, supply, {
+		from: owner,
+    });
+	
+	/// create the last phase (3)
+    await spaceToken.buySpacelens(1, { from: user, value: 1 });
+
+	/// check this phase change
+    assert.equal(
+      Number(await spaceToken.currentPhase()),
+      2,
+      "change phase per time out err"
+    );
+  });
+});
