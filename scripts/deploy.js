d637c09c93b8e5e40b91baa47daac3a3bb4d6da0
Nazhir Gonzalez Edbaissi <nazhirgonzalez@gmail.com>
2021-10-06 23:22:06 -0400

    deployed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,18 @@
+toWei = (num) => web3.utils.toWei(num);
 
 async function main() {
-    toBN = (num) => web3.utils.toBN(num + "0".repeat(18));
     // npx hardhat run scripts/deploy.js --network testnet
     
-    const SpacelensToken = ethers.getContractFactory("SpacelensToken");
-    const DummyERC20 = ethers.getContractFactory("DummyERC20");
+    const SpacelensToken = await ethers.getContractFactory("SpacelensToken");
+    const DummyERC20 = await ethers.getContractFactory("DummyERC20");
     
-    const maxSupply = toBN(10000),
-    price = 100 /** 1 ETH = 5 Tokens */,
-    min = toBN(1);
-
+    const maxSupply = toWei('10000'),
+    price = 100 /** 1 ETH = 100 Tokens */,
+    min = toWei('1'),
+    owner = (await ethers.getSigners())[0].address;
+    
     token = await DummyERC20.deploy();
+    
     space = await SpacelensToken.deploy(maxSupply, price, min, owner, token.address);
 
     await token.mint(maxSupply, {from: owner});
@@ -26,4 +28,8 @@ async function main() {
       console.error(error);
       process.exit(1);
     });
+/**
+ * SpacelensToken: 0x7eb4dB7cB1A010Bce23D0B56721ae20fdf714155
+ * DummyERC20: 0xe8E93e35e3e0bAb9C225D1D1BaC60f861cfa87A0
+ */
     
